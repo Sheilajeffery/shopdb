531bc828b441cb88580147ce9e965740d0706fd6
Sheila <siegel.sheila@gmail.com>
2016-12-15 13:20:51 +0200

    Configured applicatio.properties to use datasource with custom name, changed application so it uses a custom named datasource rather than a default

--- src/main/java/bootsample/Application.java
+++ src/main/java/bootsample/Application.java
@@ -1,13 +1,25 @@
 package bootsample;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 @SpringBootApplication
 public class Application {
 	
+	@Bean
+	@Primary
+	@ConfigurationProperties(prefix="spring.customersource")
+	public DataSource primaryDataSource() {
+	    return DataSourceBuilder.create().build();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
